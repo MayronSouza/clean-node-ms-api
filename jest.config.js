67604edfb92e249085d623fd17c07d7b1a2eaa54
MayronSouza <mayron.souza@gmz.solutions>
2022-09-27 14:07:05 -0300

    fix: remove coverage

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest',
