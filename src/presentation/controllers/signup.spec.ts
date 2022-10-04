db4de29ee32625c1990133ce625d36b854768227
MayronSouza <mayron.souza@gmz.solutions>
2022-10-04 18:54:30 -0300

    test: ensure SignUpController returns 500 if EmailValidator throws

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,7 +1,9 @@
+import { throws } from 'assert';
 import { SignUpController } from './signup';
 import { MissingParamError } from '../error/missing-param-error';
 import { InvalidParamError } from '../error/invalid-param-error';
 import { EmailValidator } from '../protocols/email-validator';
+import { InternalServerError } from '../error/internal-server-error';
 
 interface SutTypes {
   sut: SignUpController;
@@ -114,5 +116,28 @@ describe('SingUp Controller', () => {
     };
     sut.handle(httpRequest);
     expect(isValidSpy).toHaveBeenCalledWith('any_email@test.com');
+  });
+
+  test('should return 500 if EmailValidator throws', () => {
+    class EmailValidatorStub implements EmailValidator {
+      isValid(email: string): boolean {
+        throw new Error();
+      }
+    }
+
+    const emailValidatorStub = new EmailValidatorStub();
+    const sut = new SignUpController(emailValidatorStub);
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'invalid_email@test.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new InternalServerError());
   });
 });
